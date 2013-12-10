3f6d18f2dc0e510a121a433c8b4b2699ebee76b2
Hoang Quan <hoang.van.quan@framgia.com>
2013-12-10 15:46:10 +0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyShop::Application.config.secret_key_base = '8212b300f28ad6fc735cff9cc01f99e964233677467ba8c8591e63a1cfb270b395dcadd9d335ded9b2acb6e132dda53cbdee1f546288e7d741ecf334f8da8a0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyShop::Application.config.session_store :cookie_store, key: '_my_shop_session'
