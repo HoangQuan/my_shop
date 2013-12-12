28071d086ad14b188fc4133d55d96cd5baca368d
Hoang Quan <hoang.van.quan@framgia.com>
2013-12-12 09:17:49 +0700

    edit reset key facebook

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '767124323302603', 'aee9a54ef2ebc8dc0b4e0c4778652c3b'
+    provider :facebook, '767124323302603', '8bcfc00270662cb4ff0210d713ee4978'
 end
 OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
